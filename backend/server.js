54981bf44ac41c9543bd4da4e3c7926bab2a9136
Drew Costello <costeldw@mcmaster.ca>
2018-11-23 12:52:09 -0500

    first working get/post route

---
+++ backend/server.js
@@ -0,0 +1,63 @@
+// server.js
+import { getSecret } from './secrets';
+import Note from './models/note';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+
+const app = express();
+const router = express.Router();
+
+
+const API_PORT = process.env.API_PORT || 3001;
+
+require('dotenv').config();
+
+// db config
+mongoose.connect(getSecret('dbUri'));
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+// now we can set the route path & initialize the API
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+
+router.get('/notes', (req, res) => {
+  Note.find((err, notes) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: notes });
+  });
+});
+
+router.post('/notes', (req, res) => {
+  const note = new Note();
+  // body parser lets us use the req.body
+  const { author, text } = req.body;
+  if (!author || !text) {
+    // we should throw an error. we can do this check on the front end
+    return res.json({
+      success: false,
+      error: 'You must provide an author and note'
+    });
+  }
+  note.author = author;
+  note.text = text;
+  note.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// Use our router configuration when we call /api
+app.use('/api', router);
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
--- client/src/Board.js
+++ client/src/Board.js
@@ -8,8 +8,36 @@ import './Board.css';
 class Board extends Component {
   constructor() {
     super();
-    this.state = { data: [] };
+    this.state = {
+      data: [],
+      error: null,
+      author: '',
+      text: ''
+    };
+    this.pollInterval = null;
   }
+
+  componentDidMount() {
+    this.loadNotesFromServer();
+    if (!this.pollInterval) {
+      this.pollInterval = setInterval(this.loadNotesFromServer, 2000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.pollInterval) clearInterval(this.pollInterval);
+    this.pollInterval = null;
+  }
+
+  loadNotesFromServer = () => {
+    fetch('/api/notes/')
+      .then(data => data.json())
+      .then((res) => {
+        if (!res.success) this.setState({ error: res.error });
+        else this.setState({ data: res.data });
+      });
+  }
+
   render() {
     return (
       <div className="container">
@@ -17,13 +45,14 @@ class Board extends Component {
         {/* show existing notes*/}
         <div className="notes">
           <h2>Notes:</h2>
-          <NoteList data={DATA} />
+          <NoteList data={this.state.data} />
         </div>
 
         {/* for posting a new note*/}
         <div className="form">
-          <NoteForm />
+          <NoteForm author={this.state.author} text={this.state.text}/>
         </div>
+        {this.state.error && <p>{this.state.error}</p>}
       </div>
     );
   }
